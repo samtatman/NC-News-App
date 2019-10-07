5c9f4e38f3512f3f14bde2ca9142f8b32b4c54ed
Samuel Tatman <samueltatman@Samuels-MacBook-Pro.local>
2019-10-07 13:26:14 +0100

    react app created

---
+++ src/components/ArticleList.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+class ArticleList extends React.Component  {
+    state = {}
+
+
+    render () {
+    return (
+        <h2>Articles</h2>
+    )
+    }
+}
+
+export default ArticleList
---
+++ src/components/Nav.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Nav = () => {
+    return (
+        <h2>Nav placeholder</h2>
+    )
+}
+
+export default Nav
